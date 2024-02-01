fbb6934f173183c632ed696c50cb0277a1fd3fdf
Sridhar Karnati <sridhark@Sridhars-MacBook-Pro.local>
2024-02-01 18:50:25 -0500

    initial files

---
+++ App.js
@@ -0,0 +1,23 @@
+
+/** createlement will create an object. Dom.render will make use of that objecta nd convert it to html tags */
+const parent = React.createElement(
+    "div",
+     {id:"parent"},[
+        React.createElement(
+            "div",
+            {id:"child"},[React.createElement(
+                "h1", {}, "this is an heading"), React.createElement(
+                    "h2", {}, "this is an heading")]
+        ),
+        React.createElement(
+            "div",
+            {id:"child2"},[React.createElement(
+                "h1", {}, "this is an heading"), React.createElement(
+                    "h2", {}, "this is an heading")]
+        )
+     ]
+    )
+const heading = React.createElement("h1", {id:"heading"}, "Sample heading using React");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
